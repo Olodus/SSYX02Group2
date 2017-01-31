870f2412db1eb41c11be8ae16137821a71144ac5
Olodus <olof.dusterdieck@gmail.com>
2017-01-31 14:10:30 +0100

    Added a command enum in the MasterClient to abstract away the communication syntax

--- master_client/MasterClient.py
+++ master_client/MasterClient.py
@@ -1,3 +1,10 @@
+from enum import Enum
+
+class Commands(Enum):
+    getCoords = 0
+    moveForward = 1
+    rotate = 2
+
 class MasterClient():
 
     def __init__(self):
@@ -13,5 +20,11 @@ class MasterClient():
     def setNbrOfNodes(self,nbr_of_nodes):
         self.nbr_of_nodes = nbr_of_nodes
 
+    def setCommunication(com):
+        self.com = com
+
     def setFilter(self,filter):
         self.filter = filter
+
+    def setOptFunc(self,func):
+        self.opt = func
