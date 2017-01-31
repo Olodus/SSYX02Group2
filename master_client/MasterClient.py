4dd09ecf60bed2c36116ede874f658eede6a10a6
Olodus <olof.dusterdieck@gmail.com>
2017-01-31 09:30:45 +0100

    Added builder code

--- master_client/MasterClient.py
+++ master_client/MasterClient.py
@@ -1,1 +1,12 @@
 class MasterClient():
+
+    def __init__(self):
+
+    def setPositionSensor(self,sensor):
+        self.positionSensor = sensor
+
+    def setRotationSensor(self,sensor):
+        self.rotationSensor = sensor
+
+    def setNbrOfNodes(nbr_of_nodes):
+        self.nbr_of_nodes = nbr_of_nodes
--- master_client/MasterClientBuilder.py
+++ master_client/MasterClientBuilder.py
@@ -2,3 +2,14 @@ class StdMasterDirector():
 
 
 class MasterBuilder():
+
+    def __init__(self):
+        self.masterclient = MasterClient()
+
+    def buildNodes(self,nbr_of_nodes):
+        self.masterclient.setNbrOfNodes(nbr_of_nodes)
+
+    def buildUWBSensor(self):
+        uwb = UWBHandler()
+        self.masterclient.setPositionSensor(uwb)
+        self.masterclient.setRotationSensor(uwb)
