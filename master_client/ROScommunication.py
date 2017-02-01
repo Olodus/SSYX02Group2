e4b478c091e97a0158b4daf067c80f8ad5050e56
Olodus <olof.dusterdieck@gmail.com>
2017-02-01 13:53:16 +0100

    Changed the file structure a bit to make room for srv files

--- master_client/ROScommunication.py
+++ master_client/ROScommunication.py
@@ -1,2 +1,15 @@
+import rospy
+from MasterClient import Commands
 
 class ROScommunication():
+
+    def serviceCom(command):
+        c = getServiceStringFromCommand(command)
+        rospy.wait_for_service(c)
+        response = rospy.ServiceProxy(c,GetCoords)
+
+    def getServiceStringFromCommand(command):
+        if command == Commands.getCoords:
+            return str('get_coord')
+        elif command == Commands.moveForward:
+            return str('moveRobot')
