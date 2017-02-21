583ece49b2f896ba3d45678071c30d7151206675
Olodus <olof.dusterdieck@gmail.com>
2017-02-21 19:21:46 +0100

    More refactoring

--- movePath.py
+++ movePath.py
@@ -11,14 +11,6 @@ import numpy as np
 
 import roslib;
 
-
-# rospy.loginfo(rospy.get_caller_id()+"I heard %s", data.pose.pose.position.x)
-
-#p = 0
-#pub = rospy.Publisher("RosAria/cmd_vel", Twist, queue_size=1)
-#twist = Twist()
-#PI=math.pi
-
 class Robot(object):
     """docstring for Robot."""
     def __init__(self, idNumber, startPoint, startX, startY):
@@ -33,7 +25,7 @@ class Robot(object):
 		self.y = startY
 
 
-	def publishTwist(self)
+	def publishTwist(self):
 		self.pub.publish(self.twist)
 
 
@@ -92,79 +84,44 @@ def setAngle(theta_curr,theta,tempPub,tempTwist):
 
 def distanceToPoint(x,y,pointx,pointy):
     dist = math.sqrt(math.pow(pointx-x,2)+math.pow(pointy-y,2))
-    print "distance to point:"+str(dist)
     return dist
 
 def angleToPoint(x,y,pointx,pointy):
-
     angle = math.atan2(pointy-y,pointx-x)
-
     return angle
 
 def callback0(data):
 	global robot0
-	'''
-    global p
-    global pathx
-    global pathy
-	'''
+
     x = data.pose.pose.position.x
     y = data.pose.pose.position.y
     quart = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
     euler = tf.transformations.euler_from_quaternion(quart)
 
     if distanceToPoint(x,y,pathx[robot0.nextPoint],pathy[robot0.nextPoint])<=0.1:
-
 	robot0.nextPoint= (robot0.nextPoint+1)%6
-	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	print "+++++++++++++++++++ VI HAR NATT EN PUNKT ++++++++++++++++++++++"
-	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        print "next point:"+str(robot0.nextPoint)
-	rospy.sleep(1.0)
+	rospy.sleep(0.1)
 
     else:
-        #global twist
-	#global pub
 	tempAngle = angleToPoint(x, y, pathx[robot0.nextPoint], pathy[robot0.nextPoint])
 	setAngle(euler[2], tempAngle, robot0.pub, robot0.twist)
-	#print "headed towards point: "+str(p+1)
-	#twist.linear.x = 0.1
 	robot0.pub.publish(robot0.twist)
 
 def callback1(data1):
 	global robot1
-	'''
-    global p1
-    global pathx
-    global pathy
-	'''
+
     x1 = data1.pose.pose.position.x
     y1 = data1.pose.pose.position.y
     quart1 = (data1.pose.pose.orientation.x, data1.pose.pose.orientation.y, data1.pose.pose.orientation.z, data1.pose.pose.orientation.w)
     euler1 = tf.transformations.euler_from_quaternion(quart1)
-    print x1
-    print y1
 
     if distanceToPoint(x1,y1,pathx[robot1.nextPoint]+robot1.offsetX,pathy[robot1.nextPoint]+robot1.offsetY)<=0.1:
-
 	robot1.nextPoint= (robot1.nextPoint+1)%6
-	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	print "+++++++++++++++++++ VI HAR NATT EN PUNKT ++++++++++++++++++++++"
-	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        print "next point:"+str(robot1.nextPoint)
-	rospy.sleep(1.0)
+	rospy.sleep(0.1)
 
     else:
-        #global twist
-	#global pub
-	tempAngle1 = angleToPoint(x1, y1, pathx[robot1.nextPoint]robot1.offsetX, pathy[robot1.nextPoint]+robot1.offsetY)
+	tempAngle1 = angleToPoint(x1, y1, pathx[robot1.nextPoint]+robot1.offsetX, pathy[robot1.nextPoint]+robot1.offsetY)
 	setAngle(euler1[2], tempAngle1, robot1.pub, robot1.twist)
-	print "headed towards point: "+str(robot1.nextPoint)
-	#twist.linear.x = 0.1
 	robot1.pub.publish(robot1.twist)
 
 def movePath():
@@ -174,31 +131,20 @@ def movePath():
     rospy.spin()
 
 def setStartValues():
-	global robot0 = Robot(0,0,0.0,0.0)
-	global robot1 = Robot(1,4,0,-1.0)
-	'''
-    global p
-    p = 0
-    global p1
-    p1 = 3
+	global robot0
+	robot0 = Robot(0,0,0,0)
+	global robot1
+	robot1 = Robot(1,4,0,1)
+
     global pathx
     a=2
     pathx = (a, a, 0, 0, -a, -a)
     global pathy
     pathy = (0, -a, -a, a, a, 0)
-    global twist
-    twist = Twist()
-    global twist1
-    twist1 = Twist()
+
     global PI
     PI = math.pi
-    global angle
-    angle = 1
-    global pub
-    pub = rospy.Publisher("RosAria0/cmd_vel", Twist, queue_size=1)
-    global pub1
-    pub1 = rospy.Publisher("RosAria1/cmd_vel", Twist, queue_size=1)
-	'''
+
 
 if __name__ == '__main__':
     setStartValues()
