cf848dceda54cc4c78e4f29a791177f9c0d0337b
Olodus <olof.dusterdieck@gmail.com>
2017-02-22 14:56:41 +0100

    Start of a better way to sync robots

--- movePath.py
+++ movePath.py
@@ -22,6 +22,8 @@ class Robot(object):
 		self.x = startX
 		self.y = startY
 		self.waitMode = False
+		self.in_intersection = False
+		self.acc = 0.0
 
 	def publishTwist(self):
 		if not self.waitMode:
@@ -69,18 +71,21 @@ def angleToPoint(x,y,pointx,pointy):
 def callback0(data):
 	global robot0
 
-	x = data.pose.pose.position.x
-	y = data.pose.pose.position.y
+	robot0.x = data.pose.pose.position.x
+	robot0.y = data.pose.pose.position.y
 	quart = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
 	euler = tf.transformations.euler_from_quaternion(quart)
 	robot_angle = euler[2]
 
-	if distanceToPoint(x,y,pathx[robot0.nextPoint],pathy[robot0.nextPoint])<=0.2:
+	if distanceToPoint(robot0.x,robot0.y,pathx[robot0.nextPoint],pathy[robot0.nextPoint])<=0.2:
 		robot0.nextPoint= (robot0.nextPoint+1)%len(pathx)
-		if (robot0.nextPoint == intersection_point1 or robot0.nextPoint == intersection_point2) and sync_start:
-			robot0.waitMode = True
+		if (robot0.nextPoint == intersection_point1 or robot0.nextPoint == intersection_point2):
+			robot0.in_intersection = True
+			if sync_start and not intersection_ongoing:
+				robot0.waitMode = True
+				print "Waiting: robot0"
 
-	point_angle = angleToPoint(x, y, pathx[robot0.nextPoint], pathy[robot0.nextPoint])
+	point_angle = angleToPoint(robot0.x, robot0.y, pathx[robot0.nextPoint], pathy[robot0.nextPoint])
 	ang = setAngle(robot_angle, point_angle)
 	robot0.twist.angular.z = ang
 	if math.fabs(ang) > 0.2:
@@ -94,18 +99,21 @@ def callback0(data):
 def callback1(data):
 	global robot1
 
-	x = data.pose.pose.position.x
-	y = data.pose.pose.position.y
+	robot1.x = data.pose.pose.position.x
+	robot1.y = data.pose.pose.position.y
 	quart = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
 	euler = tf.transformations.euler_from_quaternion(quart)
 	robot_angle = euler[2]
 
-	if distanceToPoint(x,y,pathx[robot1.nextPoint]+robot1.offsetX,pathy[robot1.nextPoint]+robot1.offsetY)<=0.2:
+	if distanceToPoint(robot1.x,robot1.y,pathx[robot1.nextPoint]+robot1.offsetX,pathy[robot1.nextPoint]+robot1.offsetY)<=0.2:
 		robot1.nextPoint = (robot1.nextPoint+1)%len(pathx)
-		if (robot1.nextPoint == intersection_point1 or robot1.nextPoint == intersection_point2) and sync_start:
-			robot1.waitMode = True
+		if (robot1.nextPoint == intersection_point1 or robot1.nextPoint == intersection_point2):
+			robot1.in_intersection = True
+			if sync_start and not intersection_ongoing:
+				robot1.waitMode = True
+				print "Waiting: robot1"
 
-	point_angle = angleToPoint(x, y, pathx[robot1.nextPoint]+robot1.offsetX, pathy[robot1.nextPoint]+robot1.offsetY)
+	point_angle = angleToPoint(robot1.x, robot1.y, pathx[robot1.nextPoint]+robot1.offsetX, pathy[robot1.nextPoint]+robot1.offsetY)
 	ang = setAngle(robot_angle, point_angle)
 	robot1.twist.angular.z = ang
 	if math.fabs(ang) > 0.2:
@@ -116,7 +124,7 @@ def callback1(data):
 	robot1.publishTwist()
 	rospy.sleep(0.1)
 
-def movePath():
+def run_controller():
 	rospy.init_node('oodometry', anonymous=True)
 	sub = rospy.Subscriber('RosAria0/pose', Odometry, callback0)
 	sub1 = rospy.Subscriber('RosAria1/pose', Odometry, callback1)
@@ -143,11 +151,13 @@ def setStartValues():
 
 	global sync_start
 	sync_start = True
+	global intersection_ongoing
+	intersection_ongoing = False
 
 
 if __name__ == '__main__':
 	setStartValues()
 	try:
-		movePath()
+		run_controller()
 	except rospy.ROSInterruptException:
 		pass
