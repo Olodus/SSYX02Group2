9447881ea0e6d9b4b7c3aba444aed028e978f1bc
Olodus <olof.dusterdieck@gmail.com>
2017-03-22 19:03:33 +0100

    Added service files

--- controller/scripts/RobotHandler.py
+++ controller/scripts/RobotHandler.py
@@ -1,49 +1,83 @@
 
-# actions
-#  -1 : No action
+# Missions
+#  -1 : No mission
 #   0 : Go to Point
 #   1 : Follow path
 #   2 : Aim at point
+#	3 : Set speed
+#	4 : Set acceleration
 
 
 class RobotHandler(object):
-    def __init__(self, idNumber):
-		self.idNumber = idNumber
-		#self.twist = Twist()
-		self.pub = rospy.Publisher("rosaria"+str(idNumber)+"/cmd_vel", Twist, queue_size=1)
-		self.x = 0.0
-		self.y = 0.0
-		self.acc = 0.0
-		self.vx = 0.0
-		self.va = 0.0
-		self.angle = 0.0
+    def __init__(self, id_nbr):
+        rospy.init_node('robot'+str(id_nbr))
+		self.id_nbr = id_nbr
+		self.pub = rospy.Publisher("rosaria"+str(id_nbr)+"/cmd_vel", Twist, queue_size=1)
 
+		# State
+		self.state = Odometry()
 		self.executing = False
 		self.moving = False
-		self.action = -1
+
+		# Mission
+		self.mission = -1
+		self.mission_var = None
+
+		# Constants
 		self.cruising_speed = 0.3
+
+		##### Creates all services #####
+
+		# Creating 'is_ready' service
+		a = rospy.Service('robot'+str(id_nbr)+'_is_ready', Bool, self.is_ready)
+
+		# Creating 'aim_at_point' service
+		a = rospy.Service('robot'+str(id_nbr)+'_aim_at_point', Bool, self.aim_at_point)
+
+		# Creating 'go_to_point' service
+		a = rospy.Service('robot'+str(id_nbr)+'_go_to_point', Bool, self.go_to_point)
+
+		# Creating 'follow_path' service
+		a = rospy.Service('robot'+str(id_nbr)+'_follow_path', FollowPath, self.follow_path)
+
+		# Creating 'set_speed' service
+		a = rospy.Service('robot'+str(id_nbr)+'_set_speed', Bool, self.set_speed)
+
+		# Creating 'stop' service
+		a = rospy.Service('robot'+str(id_nbr)+'_stop', Bool, self.stop)
+
+		# Creating 'set_acc' service
+		a = rospy.Service('robot'+str(id_nbr)+'_set_acc', Bool, self.set_acc)
+
+		rospy.spin()
+
+
+
+    def update_state(self,data):
+		self.state = data
+
+        #self.x = data.pose.pose.x
+		#self.y = data.pose.pose.y
+		#self.vx = data.twist.linear.x
+
+	def get_state(self):
 
 
 	def run(self):
 		if executing:
-			if self.action = -1:
-				print "Error: Robot " + str(idNumber) + " is executing but has no action defined."
-			elif self.action = 0:
+			if self.mission = -1:
+				print "Error: Robot " + str(self.id_nbr) + " is executing but has no mission defined."
+			elif self.mission = 0:
 				self.
 		else:
-			print "Robot " + str(idNumber) + ": Is not doing anything"
+			print "Robot " + str(self.id_nbr) + ": Is not doing anything"
 
 	def is_ready(self):
 		return not self.executing
 
-    def update_state(self,data):
-        self.x = data.pose.pose.x
-		self.y = data.pose.pose.y
-		self.vx = data.twist.linear.x
-
 	def aim_at_point(self,point):
 		if self.executing:
-			error = "Robot " + str(idNumber) + ": Is already doing something"
+			error = "Robot " + str(self.id_nbr) + ": Is already doing something"
 			print error
 			return error
 		else:
@@ -52,56 +86,49 @@ class RobotHandler(object):
 
 	def go_to_point(point):
 		if self.executing:
-			error = "Error: Robot " + str(idNumber) + ": Is already doing something"
+			error = "Error: Robot " + str(self.id_nbr) + ": Is already doing something"
 			print error
 			return error
 		elif self.moving:
-			error = "Error: Robot " + str(idNumber) + ": Is already moving"
+			error = "Error: Robot " + str(self.id_nbr) + ": Is already moving"
 			print error
 			return error
 		else:
 			executing = True
-
-	def start_following_points(self, points):
 
 
 	def follow_path(self,points):
 		if self.executing:
-			error = "Robot " + str(idNumber) + ": Is already doing something"
+			error = "Robot " + str(self.id_nbr) + ": Is already doing something"
 			print error
 			return error
 		else:
 			executing = True
-			print "Robot " + str(idNumber) + ": Following given path
+			print "Robot " + str(self.id_nbr) + ": Following given path
 
 	# Is done when it reach the velocity
-	def move_forward_v(self,velocity):
+	def set_speed(self,velocity):
 		if self.executing:
-			error = "Robot " + str(idNumber) + ": Is already doing something"
+			error = "Robot " + str(self.id_nbr) + ": Is already doing something"
 			print error
 			return error
 		else:
-			print "Robot " + str(idNumber) + ": Moving forward with speed: " + str(velocity)
+			print "Robot " + str(self.id_nbr) + ": Moving forward with speed: " + str(velocity)
 			self.moving = True
 
-	def stopping():
+	def stop():
 		if self.executing:
-			error = "Robot " + str(idNumber) + ": Is already doing something"
+			error = "Robot " + str(self.id_nbr) + ": Is already doing something"
 			print error
 			return error
 		else:
-			print "Robot " + str(idNumber) + ": Stopping"
+			print "Robot " + str(self.id_nbr) + ": Stopping"
 
 
-	def move_forward_a(self,acc):
+	def set_acc(self,acc):
 		self.acc = acc
 
-	def abort_action(self):
-		print "Robot " + str(idNumber) + ": Aborting action"
-		self.executing = False
-		self.s
 
 	#Should only be used from inside this class. Never called from outside.
 	#Yea I know I should have hid it in some way but I don't get how Python does components...
 	def emulate_acc(self):
-        
