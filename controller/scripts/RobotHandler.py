9eba70e3e84b280911c7eb1610572cd00e1cfab5
olodus <olof.dusterdieck@gmail.com>
2017-03-28 21:13:31 +0200

    Fixed indentation problem

--- controller/scripts/RobotHandler.py
+++ controller/scripts/RobotHandler.py
@@ -9,6 +9,7 @@ import dynamic_reconfigure.client
 import math
 import rospy
 import sys
+import pprint
 
 
 # Missions
@@ -20,223 +21,166 @@ import sys
 #	4 : Set acceleration
 #	5 : Stop
 
-
-class RobotHandler(object):
-    def __init__(self, id_nbr):
-        rospy.init_node('robot'+str(id_nbr))
-        self.id_nbr = id_nbr
-        self.name = 'robot'+str(id_nbr) #TODO replace all places name could be used instead
-        self.pub = rospy.Publisher("rosaria"+str(id_nbr)+"/cmd_vel", Twist, queue_size=1)
-        self.sub = rospy.Subscriber("Filter"+str(id_nbr)+"/state", Odometry, self.update_state)
-        self.twist = Twist()
-
-		# State
-        self.state = Odometry()
-        self.executing = False
-        self.moving = False
-
-		# Mission
-        self.mission = -1
-        self.aim_point = Point()
-        self.angle_to_point = 0.0
-        self.go_point = Point()
-        self.length_to_point = 0.0
-        self.path = Path()
-        self.path_index = 0
-        self.desired_speed = 0.0
-        self.acc = 0.0
-
-		# Constants
-        self.cruising_speed = 0.3
-        self.max_speed = 1.0
-
-		##### Creates all services #####
-
-		# Creating 'is_ready' service
-        a = rospy.Service('robot'+str(id_nbr)+'_is_ready', IsReady, self.is_ready)
-
-		# Creating 'aim_at_point' service
-        a = rospy.Service('robot'+str(id_nbr)+'_aim_at_point', AimAtPoint, self.aim_at_point)
-
-		# Creating 'go_to_point' service
-        a = rospy.Service('robot'+str(id_nbr)+'_go_to_point', GoToPoint, self.go_to_point)
-
-		# Creating 'follow_path' service
-        a = rospy.Service('robot'+str(id_nbr)+'_follow_path', FollowPath, self.follow_path)
-
-		# Creating 'set_speed' service
-        a = rospy.Service('robot'+str(id_nbr)+'_set_speed', SetSpeed, self.set_speed)
-
-		# Creating 'stop' service
-        a = rospy.Service('robot'+str(id_nbr)+'_stop', Stop, self.stop)
-
-		# Creating 'set_acc' service
-        a = rospy.Service('robot'+str(id_nbr)+'_set_acc', SetAcc, self.set_acc)
-
-        rospy.spin()
+####### Do Missions ########
+# All do methods return True if they are done
+# This way all do methods can call other do methods to do part of their job
+# without having to check their "done test"
 
 
+class Robot(object):
 
     def update_state(self,data):
-		self.state = data
-		if self.mission != -1:
-			mission_done = self.do_mission()
-			self.publish_twist()
-			if mission_done:
-				self.end_mission()
+        self.state = data
+        if self.mission != -1:
+            mission_done = self.do_mission()
+            self.publish_twist()
+            if mission_done:
+                self.end_mission()
 
     def do_mission(self):
-		self.twist = Twist()
-		if self.mission == 0:
-			return do_go_to_point()
-		elif self.mission == 1:
-			return do_follow_path()
-		elif self.mission == 2:
-			return do_aim_at_point()
-		elif self.mission == 3:
-			return do_set_speed()
-		elif self.mission == 4:
-			do_set_acc()
-		elif self.mission == 5:
-			return do_stop()
+        self.twist = Twist()
+        if self.mission == 0:
+            return do_go_to_point()
+        elif self.mission == 1:
+            return do_follow_path()
+        elif self.mission == 2:
+            return do_aim_at_point()
+        elif self.mission == 3:
+            return do_set_speed()
+        elif self.mission == 4:
+            do_set_acc()
+        elif self.mission == 5:
+            return do_stop()
 
     def publish_twist(self):
-		self.pub.publish(self.twist)
+        self.pub.publish(self.twist)
 
     def end_mission(self):
         if self.mission == 0:
-			# Go to point
-			self.mission = 5
+            # Go to point
+            self.mission = 5
         elif self.mission == 1:
-			# Follow path
-			self.mission = 5
+            # Follow path
+            self.mission = 5
         elif self.mission == 2:
-			# Aim at point
-			self.mission = 5
+            # Aim at point
+            self.mission = 5
         elif self.mission == 3:
-			# Set speed
-			self.executing = False
-			self.mission = -1
-			self.moving = True
+            # Set speed
+            self.executing = False
+            self.mission = -1
+            self.moving = True
         elif self.mission == 4:
-			# Set acc
-			# dont need to do something right?
-			self.moving = True
+            # Set acc
+            # dont need to do something right?
+            self.moving = True
         elif self.mission == 5:
-			# Stop
-			self.executing = False
-			self.mission = -1
+            # Stop
+            self.executing = False
+            self.mission = -1
+
+    def get_state(self):
+        return {'state': self.state}
+
+    def is_ready(self, req):
+        return {'robot_ready': not self.executing}
+
+    def aim_at_point(self, req):
+        if self.executing:
+            error = "Error: Robot " + str(self.id_nbr) + ": Is already doing something"
+            return {'succeded': False, 'error_msg': error}
+        else:
+            self.executing = True
+            self.mission = 2
+            self.aim_point = req.point
+            msg = "Robot " + str(self.id_nbr) + ": Will aim itself towards given point"
+            return {'succeded': True, 'error_msg': msg}
+
+    def go_to_point(self, req):
+        if self.executing:
+            error = "Error: Robot " + str(self.id_nbr) + ": Is already doing something"
+            return {'succeded': False, 'error_msg': error}
+        else:
+            self.executing = True
+            self.mission = 0
+            self.go_point = req.point
+            msg = "Robot " + str(self.id_nbr) + ": Will go to given point"
+            return {'succeded': True, 'error_msg': msg}
 
 
-	def get_state(self):
-		return {'state': self.state}
+    def follow_path(self, req):
+        if self.executing:
+            error = "Robot " + str(self.id_nbr) + ": Is already doing something"
+            return {'succeded': False, 'error_msg': error}
+        else:
+            self.executing = True
+            self.mission = 1
+            self.path = req.path
+            self.path_index = 0
+            msg = "Robot " + str(self.id_nbr) + ": Will follow given path"
+            return {'succeded': True, 'error_msg': msg}
 
-	def is_ready(self, req):
-		return {'robot_ready': not self.executing}
+    def set_speed(self, req):
+        if self.executing:
+            error = "Robot " + str(self.id_nbr) + ": Can't set speed since already on a mission (use stop to abort mission)"
+            return {'succeded': False, 'error_msg': error}
+        else:
+            self.executing = True
+            self.moving = True
+            self.mission = 3
+            self.desired_speed = req.speed
+            msg = "Robot " + str(self.id_nbr) + ": Will set speed to " + str(req.speed)
+            return {'succeded': True, 'error_msg': msg}
 
-	def aim_at_point(self, req):
-		if self.executing:
-			error = "Error: Robot " + str(self.id_nbr) + ": Is already doing something"
-			return {'succeded': False, 'error_msg': error}
-		else:
-			self.executing = True
-			self.mission = 2
-			self.aim_point = req.point
-			msg = "Robot " + str(self.id_nbr) + ": Will aim itself towards given point"
-			return {'succeded': True, 'error_msg': msg}
-
-
-
-	def go_to_point(self, req):
-		if self.executing:
-			error = "Error: Robot " + str(self.id_nbr) + ": Is already doing something"
-			return {'succeded': False, 'error_msg': error}
-		else:
-			self.executing = True
-			self.mission = 0
-			self.go_point = req.point
-			msg = "Robot " + str(self.id_nbr) + ": Will go to given point"
-			return {'succeded': True, 'error_msg': msg}
-
-
-	def follow_path(self, req):
-		if self.executing:
-			error = "Robot " + str(self.id_nbr) + ": Is already doing something"
-			return {'succeded': False, 'error_msg': error}
-		else:
-			self.executing = True
-			self.mission = 1
-			self.path = req.path
-			self.path_index = 0
-			msg = "Robot " + str(self.id_nbr) + ": Will follow given path"
-			return {'succeded': True, 'error_msg': msg}
-
-	# Is done when it reach the velocity
-	def set_speed(self, req):
-		if self.executing:
-			error = "Robot " + str(self.id_nbr) + ": Can't set speed since already on a mission (use stop to abort mission)"
-			return {'succeded': False, 'error_msg': error}
-		else:
-			self.executing = True
-			self.moving = True
-			self.mission = 3
-			self.desired_speed = req.speed
-			msg = "Robot " + str(self.id_nbr) + ": Will set speed to " + str(req.speed)
-			return {'succeded': True, 'error_msg': msg}
-
-	def stop(self):
-		if self.executing:
-			error = "Robot " + str(self.id_nbr) + ": Stopped executing it's mission"
-			self.mission = 5
-			return {'succeded': True, 'error_msg': error}
-		else:
-			msg = "Robot " + str(self.id_nbr) + ": Can't stop since it wasn't executing a mission"
-			return {'succeded': False, 'error_msg': msg}
+    def stop(self):
+        if self.executing:
+            error = "Robot " + str(self.id_nbr) + ": Stopped executing it's mission"
+            self.mission = 5
+            return {'succeded': True, 'error_msg': error}
+        else:
+            msg = "Robot " + str(self.id_nbr) + ": Can't stop since it wasn't executing a mission"
+            return {'succeded': False, 'error_msg': msg}
 
 
-	def set_acc(self, req):
-		if self.executing:
-			error = "Robot " + str(self.id_nbr) + ": Can't set acc since already on a mission (use stop to abort mission)"
-			return {'succeded': False, 'error_msg': error}
-		else:
-			self.moving = True
-			self.mission = 4
-			self.acc = req.acc
-			msg = "Robot " + str(self.id_nbr) + ": Will set acc to " + str(req.acc)
-			return {'succeded': True, 'error_msg': msg}
+    def set_acc(self, req):
+        if self.executing:
+            error = "Robot " + str(self.id_nbr) + ": Can't set acc since already on a mission (use stop to abort mission)"
+            return {'succeded': False, 'error_msg': error}
+        else:
+            self.moving = True
+            self.mission = 4
+            self.acc = req.acc
+            msg = "Robot " + str(self.id_nbr) + ": Will set acc to " + str(req.acc)
+            return {'succeded': True, 'error_msg': msg}
 
-	####### Do Missions ########
-	# All do methods return True if they are done
-	# This way all do methods can call other do methods to do part of their job
-	# without having to check their "done test"
 
-	def do_go_to_point(self):
-		x = self.state.pose.pose.position.x
-		y = self.state.pose.pose.position.y
-		pointx = self.go_point.x
-		pointy = self.go_point.y
+    def do_go_to_point(self):
+        x = self.state.pose.pose.position.x
+        y = self.state.pose.pose.position.y
+        pointx = self.go_point.x
+        pointy = self.go_point.y
 
-		self.length_to_point = math.sqrt(math.pow(pointx-x,2)+math.pow(pointy-y,2))
-		length = self.length_to_point
+        self.length_to_point = math.sqrt(math.pow(pointx-x,2)+math.pow(pointy-y,2))
+        length = self.length_to_point
 
-		self.aim_point = self.go_point
-		if not do_aim_at_point():
-			return False
-		elif length >= 1.0:
-			self.desired_speed = 0.3
-			self.do_set_speed()
-			return False
-		elif length >= 0.5:
-			self.desired_speed = 0.2
-			self.do_set_speed()
-			#small_steer()
-			return False
-		elif length >= 0.25:
-			self.desired_speed = 0.1
-			self.do_set_speed()
-			return False
-		elif length >= 0.1:
-			return True
+        self.aim_point = self.go_point
+        if not do_aim_at_point():
+            return False
+        elif length >= 1.0:
+            self.desired_speed = 0.3
+            self.do_set_speed()
+            return False
+        elif length >= 0.5:
+            self.desired_speed = 0.2
+            self.do_set_speed()
+            #small_steer()
+            return False
+        elif length >= 0.25:
+            self.desired_speed = 0.1
+            self.do_set_speed()
+            return False
+        elif length >= 0.1:
+            return True
 
     def do_follow_path(self):
         if self.path_index == len(self.path):
@@ -248,93 +192,143 @@ class RobotHandler(object):
                 self.path_index = self.path_index+1
             return False
 
-	def do_aim_at_point(self):
-		x = self.state.pose.pose.position.x
-		y = self.state.pose.pose.position.y
-		pointx = self.aim_point.x
-		pointy = self.aim_point.y
-		angle = math.atan2(x-pointx,y-pointy)
-		quart = (self.state.pose.pose.orientation.x, self.state.pose.pose.orientation.y, self.state.pose.pose.orientation.z, self.state.pose.pose.orientation.w)
+    def do_aim_at_point(self):
+        x = self.state.pose.pose.position.x
+        y = self.state.pose.pose.position.y
+        pointx = self.aim_point.x
+        pointy = self.aim_point.y
+        angle = math.atan2(x-pointx,y-pointy)
+        quart = (self.state.pose.pose.orientation.x, self.state.pose.pose.orientation.y, self.state.pose.pose.orientation.z, self.state.pose.pose.orientation.w)
         euler = tf.transformations.euler_from_quaternion(quart)
         robot_angle = euler[2]
         self.angle_to_point = robot_angle - angle
-
-		# If we would make all this into a P regulator p ~= 2.0/PI probably
+        # If we would make all this into a P regulator p ~= 2.0/PI probably
 
         if math.fabs(self.angle_to_point)>20*math.PI/180:
-			if robot_angle < angle:
-				if math.fabs(self.angle_to_point)<math.PI:
-					self.twist.angular.z = 0.3
-					return False
-				else:
-					self.twist.angular.z = -0.3
-					return False
-			else:
-				if math.fabs(self.angle_to_point)<math.PI:
-					self.twist.angular.z = -0.3
-					return False
-				else:
-					self.twist.angular.z = 0.3
-					return False
-    	elif 10*math.PI/180<=math.fabs(self.angle_to_point) and math.fabs(self.angle_to_point)<=20*math.PI/180:
-			if robot_angle < angle:
-				if math.fabs(self.angle_to_point)<math.PI:
-					self.twist.angular.z = 0.1
-					return False
-				else:
-					self.twist.angular.z = -0.1
-					return False
-			else:
-				if math.fabs(self.angle_to_point)<math.PI:
-					self.twist.angular.z = -0.1
-					return False
-				else:
-					self.twist.angular.z = 0.1
-					return False
+            if robot_angle < angle:
+                if math.fabs(self.angle_to_point)<math.PI:
+                    self.twist.angular.z = 0.3
+                    return False
+                else:
+                    self.twist.angular.z = -0.3
+                    return False
+            else:
+                if math.fabs(self.angle_to_point)<math.PI:
+                    self.twist.angular.z = -0.3
+                    return False
+                else:
+                    self.twist.angular.z = 0.3
+                    return False
+        elif 10*math.PI/180<=math.fabs(self.angle_to_point) and math.fabs(self.angle_to_point)<=20*math.PI/180:
+            if robot_angle < angle:
+                if math.fabs(self.angle_to_point)<math.PI:
+                    self.twist.angular.z = 0.1
+                    return False
+                else:
+                    self.twist.angular.z = -0.1
+                    return False
+            else:
+                if math.fabs(self.angle_to_point)<math.PI:
+                    self.twist.angular.z = -0.1
+                    return False
+                else:
+                    self.twist.angular.z = 0.1
+                    return False
         elif 0.5*math.PI/180<=math.fabs(self.angle_to_point) and math.fabs(self.angle_to_point)<=10*math.PI/180:
-			if robot_angle < angle:
-				if math.fabs(self.angle_to_point)<math.PI:
-					self.twist.angular.z = 0.01
-					return False
-				else:
-					self.twist.angular.z = -0.01
-					return False
-			else:
-				if math.fabs(self.angle_to_point)<math.PI:
-					self.twist.angular.z = -0.01
-					return False
-				else:
-					self.twist.angular.z = 0.01
-					return False
-    	else:
-			self.twist.angular.z = 0.0
-			return True
+            if robot_angle < angle:
+                if math.fabs(self.angle_to_point)<math.PI:
+                    self.twist.angular.z = 0.01
+                    return False
+                else:
+                    self.twist.angular.z = -0.01
+                    return False
+            else:
+                if math.fabs(self.angle_to_point)<math.PI:
+                    self.twist.angular.z = -0.01
+                    return False
+                else:
+                    self.twist.angular.z = 0.01
+                    return False
+        else:
+            self.twist.angular.z = 0.0
+            return True
 
-	def do_set_speed(self):
-		self.twist.linear.x = self.desired_speed
-		return math.fabs(self.state.twist.twist.linear.x - self.desired_speed) <= 0.001
+    def do_set_speed(self):
+        self.twist.linear.x = self.desired_speed
+        return math.fabs(self.state.twist.twist.linear.x - self.desired_speed) <= 0.001
 
-	def do_set_acc(self):
-		self.emulate_acc()
+    def do_set_acc(self):
+        self.emulate_acc()
 
-	def do_stop(self):
-		self.twist.linear.x = 0.0
-		self.twist.angular.z = 0.0
-		return math.fabs(self.state.twist.twist.linear.x) <= 0.001 and math.fabs(self.state.twist.twist.angular.z) <= 0.001
+    def do_stop(self):
+        self.twist.linear.x = 0.0
+        self.twist.angular.z = 0.0
+        return math.fabs(self.state.twist.twist.linear.x) <= 0.001 and math.fabs(self.state.twist.twist.angular.z) <= 0.001
 
 
 
-	def emulate_acc(self):
-		#TODO Create acc implementation that handles accual time not just 0.1 sec
-		curr_speed = self.state.twist.twist.linear.x
-		desired_speed = curr_speed + self.acc*0.1
-		do_set_speed()
+    def emulate_acc(self):
+        #TODO Create acc implementation that handles accual time not just 0.1 sec
+        curr_speed = self.state.twist.twist.linear.x
+        desired_speed = curr_speed + self.acc*0.1
+        self.do_set_speed()
 
-#	def small_steer(self):
+    def __init__(self, id_nbr):
+        rospy.init_node('robot'+str(id_nbr))
+        self.id_nbr = id_nbr
+        self.name = 'robot'+str(id_nbr) #TODO replace all places name could be used instead
+        self.pub = rospy.Publisher("rosaria"+str(id_nbr)+"/cmd_vel", Twist, queue_size=1)
+        self.sub = rospy.Subscriber("Filter"+str(id_nbr)+"/state", Odometry, self.update_state)
+        self.twist = Twist()
+
+        # State
+        self.state = Odometry()
+        self.executing = False
+        self.moving = False
+
+        # Mission
+        self.mission = -1
+        self.aim_point = Point()
+        self.angle_to_point = 0.0
+        self.go_point = Point()
+        self.length_to_point = 0.0
+        self.path = Path()
+        self.path_index = 0
+        self.desired_speed = 0.0
+        self.acc = 0.0
+
+        # Constants
+        self.cruising_speed = 0.3
+        self.max_speed = 1.0
+
+        ##### Creates all services #####
+        # Creating 'is_ready' service
+        a = rospy.Service('robot'+str(id_nbr)+'_is_ready', IsReady, self.is_ready)
+
+        # Creating 'aim_at_point' service
+        b = rospy.Service('robot'+str(id_nbr)+'_aim_at_point', AimAtPoint, self.aim_at_point)
+
+        # Creating 'go_to_point' service
+        c = rospy.Service('robot'+str(id_nbr)+'_go_to_point', GoToPoint, self.go_to_point)
+
+        # Creating 'follow_path' service
+        d = rospy.Service('robot'+str(id_nbr)+'_follow_path', FollowPath, self.follow_path)
+
+        # Creating 'set_speed' service
+        e = rospy.Service('robot'+str(id_nbr)+'_set_speed', SetSpeed, self.set_speed)
+
+        # Creating 'stop' service
+        f = rospy.Service('robot'+str(id_nbr)+'_stop', Stop, self.stop)
+
+        # Creating 'set_acc' service
+        g = rospy.Service('robot'+str(id_nbr)+'_set_acc', SetAcc, self.set_acc)
+
+        rospy.spin()
+
 
 if __name__ == '__main__':
     try:
-        r = RobotHandler(int(sys.argv[1]))
+        r = Robot(int(sys.argv[1]))
 
     except rospy.ROSInterruptException:
         pass
