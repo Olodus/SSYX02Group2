fe4710d6e25c40ee05cac36af7d71d81ba00722e
olodus <olof.dusterdieck@gmail.com>
2017-03-28 21:41:08 +0200

    Implemented the PresentationProblem correctly

--- controller/scripts/PresentationProblem.py
+++ controller/scripts/PresentationProblem.py
@@ -1,59 +1,28 @@
 #!/usr/bin/env python
 
 from geometry_msgs.msg import Point
+from controller.srv import *
 import sys
+import rospy
 
 class PresentationProblem(object):
-    def __init__(self):
-        self.nbr_of_robots = 1
-        self.start_position = [Point()]
-        self.robots = []
 
-    def numberOfRobots(self):
-        return self.nbr_of_robots
-
-    def get_start_positions(self):
-        return self.start_position
-
-#    def wait_til_ready(self):
-        # loop through and see if all robots are ready
-
-    def run(self, robots):
-        if not len(robots)==self.nbr_of_robots:
-            print "Gave problem wrong number of robots"
-            print len(robots)
-            print self.nbr_of_robots
-            #Shoud probably return an error here or someting
-        else:
-            self.robots = robots
-            self.robot = robots[0]
-
-            set_up_services()
-
-            self.wait_til_ready()
-
-            #aim the robot at a ok point
-
-            #make it move with specific speed
-
-            #make it stop
-
-            #make it accelerate
-
-            #check if it reached speed
-
-            #make it deaccelerate
-
-            #check if it has stopped
+    def run(self):
+        r1_acc = rospy.ServiceProxy("Robot0/set_acc",SetAcc)
+        resp = r1_acc(0.5)
 
 
- if __name__ == '__main__':
-     try:
-         rospy.init_node("Controller")
-         r = PresentationProblem()
 
-     except rospy.ROSInterruptException:
-         pass
+if __name__ == '__main__':
+    try:
+        rospy.init_node("Controller")
+        r = PresentationProblem()
+        while True:
+            r.run()
+            rospy.sleep(1.0)
+
+    except rospy.ROSInterruptException:
+        pass
 
 # Vad som kan brytas ut i en super Problem class
 #  wait til ready metoden
