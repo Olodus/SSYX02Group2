0aa8bcb927df28407ab2c64332d66868301365f1
olodus <olof.dusterdieck@gmail.com>
2017-03-29 10:45:30 +0200

    Created a bash file to start

---
+++ start_presentation.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [[ $* == *-sim* ]]
+then
+	cd ~
+	roscore & 
+	MobileSim -r p3dx:robot1 --nomap &
+	cd catkin_ws
+	. devel/setup.bash
+	rosrun rosaria RosAria __name:="RosAria0" & 
+	sleep 1
+	rosrun controller PoseHandler.py 0 &
+	sleep 1
+	rosrun controller KalmanFilter.py 0 &
+	sleep 1
+	rosrun controller RobotHandler.py 0 &
+	sleep 1
+	rosrun controller PresentationProblem.py 
+else
+	cd ~
+	roscore & 
+	cd catkin_ws
+	. devel/setup.bash
+	rosrun rosaria RosAria __name:="RosAria0" & 
+	sleep 1
+	rosrun controller UWBPoseHandler.py 0 &
+	sleep 1
+	rosrun controller KalmanFilter.py 0 &
+	sleep 1
+	rosrun controller RobotHandler.py 0 &
+	sleep 1
+	rosrun controller PresentationProblem.py 
+fi
