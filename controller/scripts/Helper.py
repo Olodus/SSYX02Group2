5d0585c1c1f6b3757dbf04e5bc6d7a53220b887a
Olodus <olof.dusterdieck@gmail.com>
2017-04-02 21:58:04 +0200

    Added setup functions for all services

--- controller/scripts/Helper.py
+++ controller/scripts/Helper.py
@@ -15,22 +15,34 @@ class RobotServices(object):
       self.get_state = setup_get_state(robot_id)
 
 
+def setup_stop(robot_id):
+  wait_for_service("Robot"+str(robot_id)+"/stop")
+  return rospy.ServiceProxy("Robot"+str(robot_id)+"/stop",Stop)
+
+def setup_is_ready(robot_id):
+  wait_for_service("Robot"+str(robot_id)+"/is_ready")
+  return rospy.ServiceProxy("Robot"+str(robot_id)+"/is_ready",IsReady)
+
+def setup_aim_at_point(robot_id):
+  wait_for_service("Robot"+str(robot_id)+"/aim_at_point")
+  return rospy.ServiceProxy("Robot"+str(robot_id)+"/aim_at_point",AimAtPoint)
+
+def setup_go_to_point(robot_id):
+  wait_for_service("Robot"+str(robot_id)+"/go_to_point")
+  return rospy.ServiceProxy("Robot"+str(robot_id)+"/go_to_point",GoToPoint)
+
+def setup_follow_path(robot_id):
+  wait_for_service("Robot"+str(robot_id)+"/follow_path")
+  return rospy.ServiceProxy("Robot"+str(robot_id)+"/follow_path",FollowPath)
+
+def setup_set_speed(robot_id):
+  wait_for_service("Robot"+str(robot_id)+"/set_speed")
+  return rospy.ServiceProxy("Robot"+str(robot_id)+"/set_speed",SetSpeed)
+
+def setup_set_acc(robot_id):
+  wait_for_service("Robot"+str(robot_id)+"/set_acc")
+  return rospy.ServiceProxy("Robot"+str(robot_id)+"/set_acc",SetAcc)
+
 def setup_get_state(robot_id):
   wait_for_service("Robot"+str(robot_id)+"/get_state")
   return rospy.ServiceProxy("Robot"+str(robot_id)+"/get_state",GetState)
-
-def setup_stop(robot_id):
-
-def setup_is_ready(robot_id):
-
-def setup_aim_at_point(robot_id):
-
-def setup_go_to_point(robot_id):
-
-def setup_follow_path(robot_id):
-
-def setup_set_speed(robot_id):
-
-def setup_set_acc(robot_id):
-
-def setup_get_state(robot_id):
