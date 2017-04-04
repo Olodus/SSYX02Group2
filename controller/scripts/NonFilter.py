58bb826ff4ec951046bc1f085ffa8cfa3afd70ce
olodus <olof.dusterdieck@gmail.com>
2017-04-04 11:13:44 +0200

    Added a NonFilter to be used til the KF works

---
+++ controller/scripts/NonFilter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+from nav_msgs.msg import Odometry
+
+class NoFilter(object):
+    def __init__(self, robot_id):
+        rospy.init_node('Filter'+str(robot_id))
+
+        self.pub = rospy.Publisher("Filter"+str(robot_id)+"/state", Odometry, queue_size=1)
+        self.sub = rospy.Subscriber("Sensor"+str(robot_id)+"/measurement", Odometry, self.new_measurement)
+
+    def new_measurement(self, data):
+        self.pub.publish(data)
+
+if __name__ == '__main__':
+    try:
+        n = NoFilter(int(sys.argv[1]))
+        print "Filter setup done."
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        pass
