2b7694bd4089cdea28ce85b85383a7c6e60da231
olodus <olof.dusterdieck@gmail.com>
2017-04-04 11:16:15 +0200

    Made a new bash script to use til the KF works

---
+++ startNoFilter.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd ~
+roscore & 
+MobileSim -r p3dx:robot1 -r p3dx:robot2 --nomap &
+cd catkin_ws
+. devel/setup.bash
+rosrun rosaria RosAria __name:=RosAria0 _port:=localhost:8101 &
+sleep 1
+rosrun rosaria RosAria __name:=RosAria1 _port:=localhost:8102 & 
+sleep 1
+rosrun controller PoseHandler.py 0 &
+sleep 1
+rosrun controller PoseHandler.py 1 &
+sleep 1
+rosrun controller NonFilter.py 0 &
+sleep 1
+rosrun controller NonFilter.py 1 &
+sleep 1
+rosrun controller RobotHandler.py 0 &
+sleep 2
+rosrun controller RobotHandler.py 1 &
+sleep 2
+rosrun controller PriorityProblem.py 
