d6cd170062324b1d6f882a5577e441f8191343ce
Olodus <olof.dusterdieck@gmail.com>
2017-04-12 16:42:29 +0200

    Implemented DataPlotter to collect and plot data

--- controller/scripts/DataPlotter.py
+++ controller/scripts/DataPlotter.py
@@ -3,22 +3,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import rospy
+import types
+import sys
+
+class RobotData(object):
+    def __init__(self, id):
+        self.id = id
+        self.sensor_time = np.array()
+        self.sensor_data_x = np.array()
+        self.sensor_data_y = np.array()
+        self.filter_time = np.array()
+        self.filter_data_x = np.array()
+        self.filter_data_y = np.array()
+
+    def log_sensor(self, data):
+        self.sensor_time.append(data.header.stamp)
+        self.sensor_data_x.append(data.pose.pose.position.x)
+        self.sensor_data_y.append(data.pose.pose.position.y)
+
+    def log_filter(self, data):
+        self.filter_time.append(data.header.stamp)
+        self.filter_data_x.append(data.pose.pose.position.x)
+        self.filter_data_y.append(data.pose.pose.position.y)
+
 
 if __name__ == '__main__':
-    rospy.init_node('Logger')
+    try:
+        rospy.init_node('Logger')
+        nbr_of_robots = int(sys.argv[1])
+        for i in range(0,nbr_of_robots):
+            rd = RobotData(i)
+            rds[i] = rd
+            self.sensor_sub = rospy.Subscriber("Sensor"+str(i)+"/measurement", Odometry, rd.log_sensor)
+            self.filter_sub = rospy.Subscriber("Filter"+str(i)+"/state", Odometry, rd.log_filter)
 
-    self.sensor_sub = rospy.Subscriber("Sensor"+str(robot_id)+"/measurement", Odometry, log_sensor)
-    self.filter_sub = rospy.Subscriber("Filter"+str(robot_id)+"/state", Odometry, log_filter)
+        plt.axis([0, 10, 0, 10])
+        plt.ion()
 
-    plt.axis([0, 10, 0, 1])
-    plt.ion()
+        print "Logger setup done"
 
-    print "Logger setup done"
+        while True:
+            for i in range(0,nbr_of_robots):
+                plt.scatter(rds[i].sensor_data_x,rds[i].sensor_data_y,c="r")
+                plt.scatter(rds[i].filter_data_x,rds[i].filter_data_y,c="g")
+            plt.pause(0.1)
 
-    while True:
 
-        plt.pause(0.05)
-
-def log_filter(data):
-
-def log_sensor(data):
+    except rospy.ROSInterruptException:
+        pass
