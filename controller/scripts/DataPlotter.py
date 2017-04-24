1ea1220a5e2a87a8868db85d4f57d1e7a2991105
Olodus <olof.dusterdieck@gmail.com>
2017-04-24 17:50:36 +0200

    Made the DataPlotter print x and y every 5 sec

--- controller/scripts/DataPlotter.py
+++ controller/scripts/DataPlotter.py
@@ -9,45 +9,50 @@ import sys
 class RobotData(object):
     def __init__(self, id):
         self.id = id
-        self.sensor_time = np.array()
-        self.sensor_data_x = np.array()
-        self.sensor_data_y = np.array()
-        self.filter_time = np.array()
-        self.filter_data_x = np.array()
-        self.filter_data_y = np.array()
+        self.sensor_sub = rospy.Subscriber("Sensor/measurement" + str(id), Odometry, self.log_sensor)
+        self.filter_sub = rospy.Subscriber("Filter" + str(id) + "/state", Odometry, self.log_filter)
+        self.sensor_time = np.array([])
+        self.sensor_data_x = np.array([])
+        self.sensor_data_y = np.array([])
+        self.filter_time = np.array([])
+        self.filter_data_x = np.array([])
+        self.filter_data_y = np.array([])
 
     def log_sensor(self, data):
-        self.sensor_time.append(data.header.stamp)
-        self.sensor_data_x.append(data.pose.pose.position.x)
-        self.sensor_data_y.append(data.pose.pose.position.y)
+        self.sensor_time = np.append(self.sensor_time,data.header.stamp)
+        self.sensor_data_x = np.append(self.sensor_data_x,data.pose.pose.position.x)
+        self.sensor_data_y = np.append(self.sensor_data_y,data.pose.pose.position.y)
 
     def log_filter(self, data):
-        self.filter_time.append(data.header.stamp)
-        self.filter_data_x.append(data.pose.pose.position.x)
-        self.filter_data_y.append(data.pose.pose.position.y)
+        self.filter_time = np.append(self.filter_time,data.header.stamp)
+        self.filter_data_x = np.append(self.filter_data_x,data.pose.pose.position.x)
+        self.filter_data_y = np.append(self.filter_data_y,data.pose.pose.position.y)
 
 
 if __name__ == '__main__':
     try:
         nbr_of_robots = int(sys.argv[1])
         rospy.init_node('Logger')
+        rds = []
         for i in range(0,nbr_of_robots):
             rd = RobotData(i)
-            rds[i] = rd
-            self.sensor_sub = rospy.Subscriber("Sensor"+str(i)+"/measurement", Odometry, rd.log_sensor)
-            self.filter_sub = rospy.Subscriber("Filter"+str(i)+"/state", Odometry, rd.log_filter)
+            rds.append(rd)
 
-        plt.axis([0, 10, 0, 10])
-        plt.ion()
+
+        #plt.axis([0, 10, 0, 10])
+        #plt.ion()
 
         print "Logger setup done"
 
         while True:
+            rospy.sleep(5.0)
             for i in range(0,nbr_of_robots):
-                plt.scatter(rds[i].sensor_data_x[len(rds[i].sensor_data_x)],rds[i].sensor_data_y[len(rds[i].sensor_data_y)],c="r")
-                plt.scatter(rds[i].filter_data_x[len(rds[i].filter_data_x)],rds[i].filter_data_y[len(rds[i].filter_data_y)],c="g")
-            plt.pause(0.1)
-            plt.show()
+                print "Robot " + str(i) + " x: " + str(rds[i].sensor_data_x)
+                print "Robot " + str(i) + " y: " + str(rds[i].sensor_data_y)
+                #plt.scatter(rds[i].sensor_data_x[len(rds[i].sensor_data_x)],rds[i].sensor_data_y[len(rds[i].sensor_data_y)],c="r")
+                #plt.scatter(rds[i].filter_data_x[len(rds[i].filter_data_x)],rds[i].filter_data_y[len(rds[i].filter_data_y)],c="g")
+            #plt.pause(0.1)
+            #plt.show()
 
 
     except rospy.ROSInterruptException:
