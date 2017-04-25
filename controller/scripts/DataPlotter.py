2dbf0b74594c1e64252a192756e3b5d64060c2bc
Multipos3 <mikalar@student.chalmers.se>
2017-04-25 11:52:27 +0200

    Made DataPlotter save angle

--- controller/scripts/DataPlotter.py
+++ controller/scripts/DataPlotter.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import rospy
 import types
 import sys
+from nav_msgs.msg import Odometry
+import tf
+import math
 
 class RobotData(object):
     def __init__(self, id):
@@ -17,6 +20,7 @@ class RobotData(object):
         self.filter_time = np.array([])
         self.filter_data_x = np.array([])
         self.filter_data_y = np.array([])
+	self.filter_data_theta = np.array([])
 
     def log_sensor(self, data):
         self.sensor_time = np.append(self.sensor_time,data.header.stamp)
@@ -27,6 +31,10 @@ class RobotData(object):
         self.filter_time = np.append(self.filter_time,data.header.stamp)
         self.filter_data_x = np.append(self.filter_data_x,data.pose.pose.position.x)
         self.filter_data_y = np.append(self.filter_data_y,data.pose.pose.position.y)
+	quart = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(quart)
+        robot_angle = euler[2]
+	self.filter_data_theta = np.append(self.filter_data_theta,robot_angle)
 
 
 if __name__ == '__main__':
@@ -39,7 +47,7 @@ if __name__ == '__main__':
             rds.append(rd)
 
 
-        plt.axis([-5, 5, -5, 5])
+        plt.axis([-10, 10, -10, 10])
         plt.ion()
 
         print "Logger setup done"
@@ -47,12 +55,19 @@ if __name__ == '__main__':
         while True:
             rospy.sleep(5.0)
             for i in range(0,nbr_of_robots):
-                print "Robot " + str(i) + " x: " + str(rds[i].sensor_data_x)
-                print "Robot " + str(i) + " y: " + str(rds[i].sensor_data_y)
+                #print "Robot " + str(i) + " x: " + str(rds[i].sensor_data_x)
+                #print "Robot " + str(i) + " y: " + str(rds[i].sensor_data_y)
                 sensor_min_length = min(np.size(rds[i].sensor_data_x),np.size(rds[i].sensor_data_y))
                 filter_min_length = min(np.size(rds[i].filter_data_x),np.size(rds[i].filter_data_y))
+		filter_min_length = min(filter_min_length,np.size(rds[i].filter_data_theta))
                 plt.plot(rds[i].sensor_data_x[:sensor_min_length],rds[i].sensor_data_y[:sensor_min_length],c="r")
                 plt.plot(rds[i].filter_data_x[:filter_min_length],rds[i].filter_data_y[:filter_min_length],c="g")
+		print str(rds[i].filter_data_theta[filter_min_length-1])
+		#endX = 0.5*math.cos(math.radians(rds[i].filter_data_theta[filter_min_length-1]))
+		#endY = 0.5*math.sin(math.radians(rds[i].filter_data_theta[filter_min_length-1]))
+		#x = rds[i].filter_data_x[filter_min_length-1]
+		#y = rds[i].filter_data_y[filter_min_length-1]
+		#plt.plot([x,x+endX],[y,y+endY],color="b")
             plt.pause(0.1)
             plt.savefig("Path.png")
 
