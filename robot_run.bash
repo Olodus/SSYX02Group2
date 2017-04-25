b2017903029133c3da33b950cddd014eb8f0746f
ArsamShok <arsamshokrian@gmail.com>
2017-04-25 08:50:19 +0200

    Updated the bash scripts

--- robot_run.bash
+++ robot_run.bash
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-rosrun controller PoseHandler.py 0 &
+rosrun controller PoseHandler.py 0.0 &
 sleep 1
-#rosrun controller PoseHandler.py 1 &
-#sleep 1
 rosrun controller KalmanFilter.py 0 &
-#sleep 1
-#rosrun controller NonFilter.py 1 &
+sleep 1
+rosrun controller KalmanFilter.py 1 &
 sleep 1
 rosrun controller RobotHandler.py 0 &
-#sleep 1
-#rosrun controller RobotHandler.py 1 &
+sleep 1
+rosrun controller RobotHandler.py 1 &
 sleep 1
 if [[ $* == *-priority* ]]
 then 
--- start_presentation.bash
+++ start_presentation.bash
@@ -3,11 +3,11 @@ killall -9 rosmaster
 if [[ $* == *-sim_priority* ]]
 then
 	roscore &
-	MobileSim -r p3dx:robot1 --nomap & #-r p3dx:robot2 --nomap &
+	MobileSim -r p3dx:robot1 -r p3dx:robot2 --nomap &
 	rosrun rosaria RosAria __name:="RosAria0" _port:=localhost:8101 &
 	sleep 1
-	#rosrun rosaria RosAria __name:="RosAria1" _port:=localhost:8102 &
-	#sleep 1
+	rosrun rosaria RosAria __name:="RosAria1" _port:=localhost:8102 &
+	sleep 1
 	x-terminal-emulator -e bash robot_run.bash -priority
 fi
 
