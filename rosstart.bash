07d43346b6f0ecef08818f945eaa59d34078eaa2
Olodus <olof.dusterdieck@gmail.com>
2017-05-05 14:56:34 +0200

    Removed old unnecessary bash scripts

--- rosstart.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-roscore & MobileSim -r p3dx:robot1 -r p3dx:robot2 --nomap &
-cd catkin_ws
-. devel/setup.bash
-rosrun rosaria RosAria __name:=RosAria0 _port:=localhost:8101 & rosrun rosaria RosAria __name:=RosAria1 _port:=localhost:8102
